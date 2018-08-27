ae10e39777ae5364ecd47de02d693453eb8897e3
salim abdullahi <salimfaris15@gmail.com>
2018-08-27 16:18:58 +0300

    Add constructor method for player

--- moringa school/dice/js/scripts.js
+++ moringa school/dice/js/scripts.js
@@ -4,3 +4,11 @@ var player2 = "";
 var rollRandom = function() {
   return Math.floor(6*Math.random()) + 1;
 }
+
+function Player (turn){
+  this.roll = 0;
+  this.currentScore = 0;
+  this.score= 0;
+  this.turn = turn;
+  this.playerName;
+}
