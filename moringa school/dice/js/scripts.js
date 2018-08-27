cc4298e951b3ae90562d7cdfd43a3bfe1804f16f
salim abdullahi <salimfaris15@gmail.com>
2018-08-27 16:31:25 +0300

    Add front end logic code to start game and start new game

--- moringa school/dice/js/scripts.js
+++ moringa school/dice/js/scripts.js
@@ -46,3 +46,33 @@ var clearValues = function(){
 }
 
 $(document).ready(function(){
+  $("button#playGame").click(function(event){
+  player1 = new Player(true);
+  player2 = new Player(false);
+  $(".player-space").show();
+  $(".start-game").hide();
+
+  var player1Name = $(".player1Name").val();
+  $("#player1Name").text(player1Name);
+
+  var player2Name = $(".player2Name").val();
+  $("#player2Name").text(player2Name);
+
+  player1.playerName = player1Name;
+  player2.playerName = player2Name;
+  });
+
+  $("button#new-game").click(function(event){
+    $(".player-space").hide();
+    clearValues();
+    player1.newGame();
+    player2.newGame();
+    $("#round-total-1").empty();
+    $("#total-score-1").empty();
+    $("die-roll-1").empty();
+    $("#round-total-2").empty();
+    $("#total-score-2").empty();
+    $("#die-roll-2").empty();
+
+    $(".start-game").show();
+  });
